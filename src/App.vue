9b6ccdbc2e4dddee8cd15c168cd7027d3dcc458f
alix2018 <stephanie.alix@icemobile.com>
2021-05-15 17:31:58 +0200

    home page for new less waste website

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <HomePage message="Work in progress :)"/>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HomePage,
   },
 };
 </script>
 
 <style>
-#app {
+body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  color: #272D2D;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+</style>
+
+<style scoped>
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <img v-show="showGreenLogo" @click="changeLogo()" alt="Less Waste green logo" src="../assets/less-waste-logo.svg">
+    <img v-show="showYellowLogo" @click="changeLogo()" alt="Less Waste yellow logo" src="../assets/less-waste-logo-yellow.svg">
+    <h1 @click="consoleLogHello()" :class="[addClassSomething]">{{message}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  created() {
+    console.log('Home component created');
+  },
+  mounted() {
+    console.log('Home component mounted');
+  },
+  data() {
+    return {
+      showGreenLogo: true,
+      showYellowLogo: false,
+    };
+  },
+  mixins: [],
+  computed: {
+    addClassSomething() {
+      return 'new-class-here';
+    },
+  },
+  methods: {
+    consoleLogHello() {
+      console.log('Hellooooo!');
+    },
+    changeLogo() {
+      console.log('Change logo :)');
+      this.showGreenLogo = !this.showGreenLogo;
+      this.showYellowLogo = !this.showYellowLogo;
+    },
+  },
+  props: {
+    message: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+div {
+  img {
+    width: 250px;
+    height: 250px;
+    cursor: pointer;
+  }
+
+  h1 {
+    margin-top: 50px;
+  }
+}
+</style>
