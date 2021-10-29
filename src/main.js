6e704e78985f9e0d3e136268efbbddcacae63885
alix2018 <stephanie.alix@icemobile.com>
2021-10-29 21:21:48 +0200

    start vuex

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import VueGtag from 'vue-gtag';
 import i18n from './i18n';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 const gtagId = process.env.NODE_ENV === 'production' ? 'UA-147425189-3' : '';
 
 createApp(App)
   .use(router)
+  .use(store)
   .use(VueGtag, {
     config: { id: gtagId }
   })
