3ce9e8fd11fe30a7ead0d4706634019de245969b
alix2018 <stephanie.alix@icemobile.com>
2021-11-29 20:37:28 +0100

    setup french text for newsletter form + presentation example

--- src/components/NewsletterForm.vue
+++ src/components/NewsletterForm.vue
@@ -8,7 +8,7 @@
               <path d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z" />
             </svg>
             <span class="sib-form-message-panel__inner-text">
-              Your subscription could not be saved. Please try again.
+              {{ $t('newsletter.form_error_message') }}
             </span>
           </div>
         </div>
@@ -19,7 +19,7 @@
               <path d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z" />
             </svg>
             <span class="sib-form-message-panel__inner-text">
-              Your subscription has been successful.
+              {{ $t('newsletter.form_success_message') }}
             </span>
           </div>
         </div>
@@ -28,14 +28,14 @@
           <form id="sib-form" method="POST" action="https://3cf400b4.sibforms.com/serve/MUIEAD1g6kLimFaRptXe3hq5EUXQbXxypoIL-zygjWzg17ngee6xnzUTRpoA99V8LlXtOgcLUq2cUCfhtLERRJvGWJZCtU7aMP9Pi5d8P0Lkzp-WuyC4_1-wMav755fvyzyb4D2opIKwGU3_nahFB_mAr_ALVS36TZCbtNjQXRTZUaKDqEoai7_KqMPcDIbye9MWXI1F_tn-C2Ev" data-type="subscription">
             <div style="padding: 8px 0;">
               <div class="sib-form-block flex" style="font-size:32px; text-align:left; font-weight:700; color:#272D2D; background-color:transparent;">
-                <p>Newsletter</p>
+                <p class="title">{{ $t('newsletter.form_title') }}</p>
                 <img src="../assets/cross.svg" height="18" @click="onClickCloseModal" alt="cross">
               </div>
             </div>
             <div style="padding: 8px 0;">
               <div class="sib-form-block" style="font-size:16px; text-align:left; color:#272D2D; background-color:transparent;">
                 <div class="sib-text-form-block">
-                  <p>Subscribe to our newsletter and stay updated.</p>
+                  <p>{{ $t('newsletter.from_subtitle') }}</p>
                 </div>
               </div>
             </div>
@@ -44,7 +44,7 @@
                 <div class="form__entry entry_block">
                   <div class="form__label-row ">
                     <label class="entry__label" style="font-size:16px; text-align:left; font-weight:700; color:#272D2D;" for="EMAIL" data-required="*">
-                      Enter your email address to subscribe
+                      {{ $t('newsletter.form_label_email') }}
                     </label>
 
                     <div class="entry__field">
@@ -55,7 +55,7 @@
                   <label class="entry__error entry__error--primary" style="font-size:16px; text-align:left; color:#661d1d; background-color:#ffeded; border-radius: 6px; border-color:#ff4949;">
                   </label>
                   <label class="entry__specification" style="font-size:12px; text-align:left; color:#8390A4;">
-                    Provide your email address to subscribe. For e.g abc@xyz.com
+                    {{ $t('newsletter.form_email_example') }}
                   </label>
                 </div>
               </div>
@@ -67,13 +67,15 @@
                     <div class="entry__choice">
                       <label>
                         <input type="checkbox" class="input_replaced" value="1" id="OPT_IN" name="OPT_IN" @click="onClickCheckbox"/>
-                        <span class="checkbox checkbox_tick_positive"></span><span style="font-size:14px; text-align:left; color:#272D2D; background-color:transparent;"><p>I agree to receive your newsletters and accept the data privacy statement.</p></span> </label>
+                        <span class="checkbox checkbox_tick_positive"></span><span style="font-size:14px; text-align:left; color:#272D2D; background-color:transparent;">
+                          <p>{{ $t('newsletter.form_accept_terms') }}</p>
+                        </span></label>
                     </div>
                   </div>
                   <label class="entry__error entry__error--primary" style="font-size:16px; text-align:left; color:#661d1d; background-color:#ffeded; border-radius: 6px; border-color:#ff4949;">
                   </label>
                   <label class="entry__specification" style="font-size:12px; text-align:left; color:#8390A4;">
-                    You may unsubscribe at any time using the link in our newsletter.
+                    {{ $t('newsletter.form_unsubscribe') }}
                   </label>
                 </div>
               </div>
@@ -93,7 +95,7 @@
                   </svg>
                 </div>
                 <p style="font-size:14px; text-align:left; color:#687484; background-color:transparent;">
-                  We use Sendinblue as our marketing platform. By Clicking below to submit this form, you acknowledge that the information you provided will be transferred to Sendinblue for processing in accordance with their <a target="_blank" class="clickable_link" href="https://www.sendinblue.com/legal/termsofuse/">terms of use</a>
+                  {{ $t('newsletter.form_sendingblue_terms') }}<a target="_blank" class="clickable_link" href="https://fr.sendinblue.com/legal/termsofuse/">{{ $t('newsletter.form_sendingblue_terms_link') }}</a>.
                 </p>
               </div>
 
@@ -104,13 +106,13 @@
                   <svg class="icon clickable__icon progress-indicator__icon sib-hide-loader-icon" viewBox="0 0 512 512">
                     <path d="M460.116 373.846l-20.823-12.022c-5.541-3.199-7.54-10.159-4.663-15.874 30.137-59.886 28.343-131.652-5.386-189.946-33.641-58.394-94.896-95.833-161.827-99.676C261.028 55.961 256 50.751 256 44.352V20.309c0-6.904 5.808-12.337 12.703-11.982 83.556 4.306 160.163 50.864 202.11 123.677 42.063 72.696 44.079 162.316 6.031 236.832-3.14 6.148-10.75 8.461-16.728 5.01z" />
                   </svg>
-                  SUBSCRIBE
+                  {{ $t('newsletter.form_btn_submit') }}
                 </button>
               </div>
             </div>
 
             <input type="text" name="email_address_check" value="" class="input--hidden">
-            <input type="hidden" name="locale" value="en">
+            <input type="hidden" name="locale" value="fr">
           </form>
         </div>
       </div>
@@ -163,22 +165,12 @@ export default {
   padding: 28px;
 }
 
-#sib-container input:-ms-input-placeholder {
+#sib-container input:-ms-input-placeholder,
+#sib-container input::placeholder,
+#sib-container textarea::placeholder  {
   text-align: left;
-  font-family: "Helvetica", sans-serif;
-  color: #c0ccda;
-}
-
-#sib-container input::placeholder {
-  text-align: left;
-  font-family: "Helvetica", sans-serif;
-  color: #c0ccda;
-}
-
-#sib-container textarea::placeholder {
-  text-align: left;
-  font-family: "Helvetica", sans-serif;
-  color: #c0ccda;
+  font-family: 'Lato Regular';
+  color: #C0CCDA;
 }
 
 .sib-form {
@@ -190,6 +182,7 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-bottom: 8px;
 }
 
 div.modal.hidden {
@@ -217,6 +210,13 @@ label {
 
 button[disabled] {
   opacity: 0.5;
+  text-transform: uppercase;
   pointer-events: none;
+}
+
+p.title {
+  font-family: 'Lato Black';
+  font-size: 22px;
+  text-transform: uppercase;
 }
 </style>
