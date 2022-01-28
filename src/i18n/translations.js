a8041ebf0b78c9e54857ad07945c26190fda333c
Stéphanie Alix <stephanie@MacSteph.local>
2022-01-28 17:03:12 +0100

    translate website in English and Dutch

---
+++ src/i18n/translations.js
@@ -0,0 +1,176 @@
+export default {
+  en: {
+    home: {
+      title: 'Tips to reduce your waste',
+      subtitle: 'Coming Soon',
+      h2_first_section_title: 'Easy, simple and effective information',
+      article_title_time: 'Do you want to save time?',
+      article_text_time: 'There\'s no too much talking here, we provide you with the key points so that you can progress at your own pace.',
+      article_title_info: 'You don\'t know where to look?',
+      article_text_info: 'Don\'t worry. We will help you reduce your waste for each room in your house.',
+      article_title_environment: 'Do you want to preserve your health?',
+      article_text_environment: '... And the one from the planet. We give you the behaviors to avoid in order to take care of yourself and the environment.',
+      article_title_money: 'You want to save money?',
+      article_text_money: 'We give you tips to relieve your wallet while reducing your impact.',
+      newsletter_coming_soon: 'Have you answered <span class="highlight">"yes"</span> to all the previous questions?',
+      newsletter_inscription: 'Then subscribe to our newsletter to be informed about what\'s next!',
+      newsletter_button_subscribe: 'Subscribe',
+      about_us_who_we_are: 'Who are we?',
+      about_us_description: 'Hello to you who is sitting quietly in front of your screen. We are <span class="highlight">Shannon & Stéphanie</span>, respectively Belgian and French living in Belgium and the Netherlands. Yes, it\'s quite a mix.\n\nYou\'re probably wondering why we made this website? We actually both work to <span class="highlight">reducing our waste</span> and we quickly had the same frustrations and the same crappy experiences, I promise we\'ll tell you!\n\n So we would like to take you on <span class="highlight">our zero waste adventure</span> and help you develop yours (whether you are just starting out or already have experience). \'experience). What we want is to make your experience as pleasant as possible by providing you with the information you need <span class="highlight">easily</span>. And then why not get your friends on board too because alone we wouldn\'t go very far to save our beautiful planet. 🌍',
+      social_in_the_meantime: 'You want to know more?',
+      social_follow_us: 'Follow us on social media!',
+      social_instagram: 'Instagram',
+      social_facebook: 'Facebook',
+      share_title: 'You want to share with your friends?',
+      share_description: 'Send them this link, and don\'t forget your family, your colleagues, your neighbour, your cat, your dog... So that we can all act on our scale!',
+      language_en: 'English',
+      language_fr: 'French',
+      language_nl: 'Dutch',
+      pluralExample: 'no element | {count} element | {count} elements'
+    },
+    newsletter: {
+      form_error_message: 'Unfortunately your registration could not be saved. Try again 🤞',
+      form_success_message: 'Your registration went well, welcome aboard! 🎉',
+      form_title: 'Newsletter',
+      from_subtitle: 'Are you looking forward to the next step too? 🙂',
+      form_label_email: 'Enter your email address to register',
+      form_email_input_example: 'Enter your email address to subscribe. For example: abc{\'@\'}xyz.com',
+      form_accept_terms: 'I agree to receive your newsletter and I accept the data privacy statement.',
+      form_unsubscribe: 'You can unsubscribe at any time using the link in our newsletter.',
+      form_sendingblue_terms: 'We use Sendinblue as a marketing platform. By clicking below to submit this form, you acknowledge that the information you have provided will be transferred to Sendinblue for processing in accordance with their ',
+      form_sendingblue_terms_link: 'terms of use',
+      form_btn_submit: 'Confirm',
+      confirmation_title: 'Congratulations!',
+      confirmation_text1: 'You are subscribed to our newsletter, we will contact you when our site is ready 🙂',
+      confirmation_text2: 'Waiting to hear from us, share with your friends!'
+    },
+    share: {
+      button_copy: 'Copy URL',
+      button_native_share: 'Share',
+    },
+    privacyPolicy: {
+      btn_back_to_home: 'Back to home'
+    },
+    footer: {
+      comment_ideas: 'Do you have any comments or ideas? Contact us!',
+      copyright: '© {year}. All rights reserved.',
+      privacy_policy: 'Privacy Policy'
+    }
+  },
+  fr: {
+    home: {
+      title: 'Des conseils pour réduire tes déchets',
+      subtitle: 'Bientôt disponible',
+      h2_first_section_title: 'Des informations faciles, simples et efficaces',
+      article_title_time: 'Tu as envie de gagner du temps&#8239;?',
+      article_text_time: 'Ici pas de blabla, on te fournit les points clés pour que  tu puisses avancer à ton rythme.',
+      article_title_info: 'Tu ne sais plus où donner de la tête&#8239;?',
+      article_text_info: 'Ne t\'inquiète pas. On va t\'aider à réduire tes déchets pour chacunes des pièces de ta maison.',
+      article_title_environment: 'Tu veux préserver ta santé&#8239;?',
+      article_text_environment: '... Et celle de la planète. On te donne les comportements à éviter pour prendre soin de toi et de l\'environnement.',
+      article_title_money: 'Tu souhaites faire des économies&#8239;?',
+      article_text_money: 'On te donne des astuces pour soulager ton porte-monnaie tout en réduisant ton impact.',
+      newsletter_coming_soon: 'Tu as répondu <span class="highlight">« oui »</span> à toutes les questions précédentes ?',
+      newsletter_inscription: 'Alors inscris-toi à notre newsletter pour être informé de la suite !',
+      newsletter_button_subscribe: 'S\'abonner',
+      about_us_who_we_are: 'Qui sommes-nous ?',
+      about_us_description: 'Bonjour à toi qui est assis bien sagement devant ton écran. Nous sommes <span class="highlight">Shannon & Stéphanie</span>, respectivement belge et française vivant en Belgique et aux Pays-Bas. Oui, ça fait un sacré mix.\n\nTu te demandes sûrement pourquoi on a fait ce site ? Figure toi qu\'on travaille toutes les 2 à la <span class="highlight">réduction de nos déchets</span> et on a vite eu les mêmes frustrations et les mêmes expériences foireuses, promis on te racontera !\n\nDu coup on aimerait t\'embarquer dans <span class="highlight">notre aventure zero waste</span> et t\'aider à développer la tienne (que tu en sois à tes débuts ou que tu aies déjà de l\'expérience). Ce qu\'on veut c\'est rendre ton expérience la plus sympa possible en te fournissant les infos dont tu as besoin <span class="highlight">facilement</span>.  Et puis pourquoi pas embarquer tes potes aussi car tout seul on n\'irait pas très loin pour sauver notre belle planète. 🌍',
+      social_in_the_meantime: 'Tu veux en savoir plus ?',
+      social_follow_us: 'Suis-nous sur les réseaux sociaux&#8239;!',
+      social_instagram: 'Instagram',
+      social_facebook: 'Facebook',
+      share_title: 'Envie de partager avec tes potes ?',
+      share_description: 'Envoie-leur ce lien, et n\'oublie pas ta famille, tes collègues, ton voisin, ton chat, ton chien... Pour qu\'on puisse tous agir à notre échelle !',
+      language_en: 'Anglais',
+      language_fr: 'Français',
+      language_nl: 'Néerlandais',
+      pluralExample: 'pas d\'élément | {count} élément | {count} éléments'
+    },
+    newsletter: {
+      form_error_message: 'Malheureusement ton inscription n\'a pas pu être enregistrée. Essaie de nouveau 🤞',
+      form_success_message: 'Ton inscription à bien marché, bienvenue parmis nous ! 🎉',
+      form_title: 'Newsletter',
+      from_subtitle: 'Toi aussi tu as hâte de connaître la prochaine étape ? 🙂',
+      form_label_email: 'Entre ton adresse e-mail pour t\'inscrire',
+      form_email_input_example: 'Indique ton adresse e-mail pour t\'abonner. Par exemple: abc{\'@\'}xyz.com',
+      form_accept_terms: 'J\'accepte de recevoir votre newsletter et j\'accepte la déclaration de confidentialité des données.',
+      form_unsubscribe: 'Tu peux te désinscrire à tout moment en utilisant le lien figurant dans notre newsletter.',
+      form_sendingblue_terms: 'On utilise Sendinblue comme plateforme de marketing. En cliquant ci-dessous pour soumettre ce formulaire, tu reconnais que les informations que tu as fournies seront transférées à Sendinblue pour être traitées conformément à leur ',
+      form_sendingblue_terms_link: 'conditions d\'utilisation',
+      form_btn_submit: 'Confirmer',
+      confirmation_title: 'Félicitations !',
+      confirmation_text1: 'Tu es abonné à notre newsletter, on te contactera quand notre site sera prêt 🙂',
+      confirmation_text2: 'En attendant d\'avoir de nos nouvelles, partage avec tes amis&#8239;!'
+    },
+    share: {
+      button_copy: 'Copier l\'URL',
+      button_native_share: 'Partager',
+    },
+    privacyPolicy: {
+      btn_back_to_home: 'Retour à l\'accueil'
+    },
+    footer: {
+      comment_ideas: 'Tu as des commentaires ou des idées&#8239;? Contacte-nous&#8239;!',
+      copyright: '© {year}. Tous droits réservés.',
+      privacy_policy: 'Politique de confidentialité'
+    }
+  },
+  nl: {
+    home: {
+      title: 'Tips om je afval te verminderen',
+      subtitle: 'Binnenkort',
+      h2_first_section_title: 'Eenvoudige, eenvoudige en effectieve informatie',
+      article_title_time: 'Wil je tijd besparen?',
+      article_text_time: 'Er is hier geen sprake van, we geven je de belangrijkste punten zodat je in je eigen tempo vooruitgang kunt boeken.',
+      article_title_info: 'Weet je niet waar je heen moet?',
+      article_text_info: 'Maak je geen zorgen. Wij helpen u uw afval voor elke kamer in uw huis te verminderen.',
+      article_title_environment: 'Wilt u uw gezondheid behouden?',
+      article_text_environment: '...En dat van de planeet. We geven je het gedrag dat je moet vermijden om voor jezelf en het milieu te zorgen.',
+      article_title_money: 'Wilt u geld besparen?',
+      article_text_money: 'We geven je tips om je portemonnee te ontlasten en tegelijkertijd je impact te verkleinen.',
+      newsletter_coming_soon: 'Heb je <span class="highlight">"ja"</span> beantwoord op alle voorgaande vragen?',
+      nieuwsbrief_inscription: 'Abonneer je dus op onze nieuwsbrief om op de hoogte te blijven van wat er gaat komen!',
+      newsletter_button_subscribe: 'Abonneren',
+      about_us_who_we_are: 'Wie zijn wij?',
+      about_us_description: 'Hallo aan u die rustig voor uw scherm zit. Wij zijn <span class="highlight">Shannon & Stéphanie</span>, respectievelijk Belg en Frans woonachtig in België en Nederland. Ja, het is nogal een mix.\n\nJe vraagt je waarschijnlijk af waarom we deze site hebben gemaakt? Stel je voor dat we allebei werken aan <span class="highlight">het verminderen van ons afval</span> en dat we al snel dezelfde frustraties en dezelfde waardeloze ervaringen hadden, dat beloof ik je!\n\n Dus we zouden graag nemen je mee op <span class="highlight">ons zero waste-avontuur</span> en helpen je om het jouwe te ontwikkelen (of je nu net begint of al ervaring hebt). \'ervaring). We willen uw ervaring zo aangenaam mogelijk maken door u <span class="highlight">gemakkelijk</span> de informatie te verstrekken die u nodig heeft. En waarom zou je dan niet ook je vrienden aan boord krijgen, want alleen zouden we niet ver gaan om onze prachtige planeet te redden. ',
+      social_in_the_meantime: 'Meer weten?',
+      social_follow_us: 'Volg ons op sociale media!',
+      social_instagram: 'Instagram',
+      social_facebook: 'Facebook',
+      share_title: 'Wil je delen met je vrienden?',
+      share_description: 'Stuur ze deze link, en vergeet je familie, je collega\'s, je buurman, je kat, je hond niet... Zodat we allemaal op onze schaal kunnen handelen!',
+      language_en: 'Engels',
+      language_fr: 'Frans',
+      language_nl: 'Nederlands',
+      meervoudVoorbeeld: 'geen element | {tel} element | {tel} elementen'
+    },
+    newsletter: {
+      form_error_message: 'Helaas kon uw registratie niet worden opgeslagen. Probeer het opnieuw 🤞',
+      form_success_message: 'Je registratie is goed verlopen, welkom aan boord! ',
+      form_title: 'Nieuwsbrief',
+      from_subtitle: 'Kijk jij ook uit naar de volgende stap? ',
+      form_label_email: 'Vul uw e-mailadres in om u te registreren',
+      form_email_input_example: 'Vul uw e-mailadres in om u te abonneren. Bijvoorbeeld: abc{\'@\'}xyz.com',
+      form_accept_terms: 'Ik ga akkoord met het ontvangen van uw nieuwsbrief en accepteer de privacyverklaring.',
+      form_unsubscribe: 'U kunt zich op elk moment afmelden via de link in onze nieuwsbrief.',
+      form_sendingblue_terms: 'Wij gebruiken Sendinblue als marketingplatform. Door hieronder te klikken om dit formulier in te dienen, erkent u dat de door u verstrekte informatie zal worden overgedragen aan Sendinblue voor verwerking in overeenstemming met hun ',
+      form_sendingblue_terms_link: 'gebruiksvoorwaarden',
+      form_btn_submit: 'Bevestigen',
+      confirm_title: 'Gefeliciteerd!',
+      confirm_text1: 'Je bent ingeschreven voor onze nieuwsbrief, we nemen contact met je op als onze site klaar is 🙂',
+      confirm_text2: 'Wachten op iets van ons, deel het met je vrienden!'
+    },
+    share: {
+      button_copy: 'Kopieer URL',
+      button_native_share: 'Delen',
+    },
+    privacyPolicy: {
+      btn_back_to_home: 'Terug naar huis'
+    },
+    footer: {
+      comment_ideas: 'Heeft u opmerkingen of ideeën? Neem contact met ons op&#8239;!',
+      copyright: '© {jaar}. Alle rechten voorbehouden.',
+      privacy_policy: 'Privacybeleid'
+    }
+  }
+};
