f0e05756e5f3729543e5d36606692ca453d0531a
Stéphanie Alix <stephanie@MacSteph.local>
2022-01-28 15:34:09 +0100

    determine default language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -85,8 +85,34 @@ const messages = {
   }
 };
 
+const determineDefaultLanguage = () => {
+  let defaultLanguage = 'en';
+  const userLanguages = navigator.languages;
+  const websiteAvailableLanguages = [];
+
+  // Get all the supported languages
+  Object.keys(messages).forEach((lang) => {
+    websiteAvailableLanguages.push(lang);
+  });
+
+  // Determine the highest language supported
+  // Ex: en-nl will select English language
+  userLanguages.every((userLanguage) => {
+    const [languageCode] = userLanguage.split('-');
+    if (websiteAvailableLanguages.includes(languageCode)) {
+      defaultLanguage = languageCode;
+      // Stop the loop when language matches
+      return false;
+    }
+
+    return true;
+  });
+
+  return defaultLanguage;
+};
+
 export default createI18n({
-  locale: 'fr',
+  locale: determineDefaultLanguage(),
   legacy: false,
   globalInjection: true, // Inject i18n properties and function to all components for options API
   messages
