ab301461f949f52f607265a97e762207d86e9224
alix2018 <stephanie.alix95@gmail.com>
2022-02-05 16:49:56 +0100

    add metadata in the 3 languages

--- server.js
+++ server.js
@@ -11,23 +11,44 @@ const indexTemplate = Handlebars.compile(indexHbs.toString());
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Metadata translations
 const metadata = {
   en: {
-    title: 'Tips to easily reduce your waste'
+    title: 'Tips to easily reduce your waste',
+    description: 'You want to start reducing your waste? Or you already are active in the zero waste and want to get easy tips and tricks? We have the solution!'
+
   },
   fr: {
-    title: 'Des conseils pour réduire tes déchets facilement'
+    title: 'Des conseils pour réduire tes déchets facilement',
+    description: 'Tu veux commencer à réduire tes déchets? Ou tu es déjà actif dans le zero waste et tu veux avoir des conseils et astuces facilement? Nous avons la solution !'
   },
   nl: {
-    title: 'Tips om je afval gemakkelijk te verminderen'
+    title: 'Tips om je afval gemakkelijk te verminderen',
+    description: 'Wil je beginnen met het verminderen van je afval? Of ben je al actief in de zero waste en wil je makkelijke tips en tricks? Wij hebben de oplossing!'
   }
 };
 
-app.get('/:name', (req, res) => {
-  const currentLanguage = metadata[req.query.hl];
+app.get('/*', (req, res) => {
+  const language = req.query.hl;
+  const languageTranslations = metadata[language];
+
+  let languageCode;
+  switch (language) {
+    case 'fr':
+      languageCode = 'fr_FR';
+      break;
+    case 'nl':
+      languageCode = 'nl_NL';
+      break;
+    default:
+      languageCode = 'en_GB';
+  }
+
   const data = {
-    name: req.path.split('/')[1],
-    title: currentLanguage.title
+    language: req.query.hl,
+    title: languageTranslations.title,
+    description: languageTranslations.description,
+    languageCode
   };
   res.send(indexTemplate(data));
 });
