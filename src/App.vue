6891a5c69f1137994d36403c71222189d3500d30
alix2018 <stephanie.alix95@gmail.com>
2022-10-09 16:00:04 +0200

    Setup project with vue3, TS and Vite

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,85 @@
-<template>
-  <main>
-    <router-view/>
-  </main>
-</template>
-
-<script>
-export default {
-  name: 'App',
-  mounted() {
-    this.$gtag.event('start_application', { location: window.location.pathname });
-  }
-};
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style lang="scss">
-@import "@scss/_global.scss";
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-@font-face {
-  font-family: 'Lato Thin';
-  src: url(./assets/fonts/Lato-Thin.woff) format('woff');
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
 }
 
-@font-face {
-  font-family: 'Lato Light';
-  src: url(./assets/fonts/Lato-Light.woff) format('woff');
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
 }
 
-@font-face {
-  font-family: 'Lato Regular';
-  src: url(./assets/fonts/Lato-Regular.woff) format('woff');
-}
-
-@font-face {
-  font-family: 'Lato Italic';
-  src: url(./assets/fonts/Lato-Italic.woff) format('woff');
-}
-
-@font-face {
-  font-family: 'Lato Bold';
-  src: url(./assets/fonts/Lato-Bold.woff) format('woff');
-}
-
-@font-face {
-  font-family: 'Lato Black';
-  src: url(./assets/fonts/Lato-Black.woff) format('woff');
-}
-
-body {
-  font-family: 'Lato Regular', Helvetica, sans-serif;
-  color: $color-black;
+nav {
+  width: 100%;
+  font-size: 12px;
   text-align: center;
-  margin: 0;
-  padding: 0 $body-padding-big-screen;
-  font-size: 20px;
+  margin-top: 2rem;
 }
 
-@media (max-width: 1500px) {
-  body {
-    padding: 0 $body-padding-desktop;
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
   }
-}
 
-@media (max-width: 1023px) {
-  body {
-    padding: 0 $body-padding-mobile;
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
