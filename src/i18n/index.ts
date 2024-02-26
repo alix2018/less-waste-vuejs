7f78cd9f25f0ecbfb5c774aa62ca0629a9bcafe6
alix2018 <stephanie.alix95@gmail.com>
2024-02-26 16:23:36 +0100

    Add i18n + translations

---
+++ src/i18n/index.ts
@@ -0,0 +1,36 @@
+import { createI18n } from 'vue-i18n';
+import { translations, defaultLanguage, availableLanguages } from './translations';
+
+const determineDefaultLanguage = () => {
+  let currentDefaultLanguage = defaultLanguage;
+  const userLanguages = navigator.languages;
+  const websiteAvailableLanguages = availableLanguages();
+
+  const urlParams = new URL(document.location.href).searchParams;
+  const savedLanguage = urlParams.get('hl') || localStorage.getItem('language') || '';
+  if (websiteAvailableLanguages.includes(savedLanguage)) {
+    currentDefaultLanguage = savedLanguage;
+  } else {
+    // TODO: improve the every function
+    userLanguages.every((userLanguage) => {
+      // Determine the highest language supported. Ex: en-nl will select English language
+      const [languageCode] = userLanguage.split('-');
+      if (websiteAvailableLanguages.includes(languageCode)) {
+        currentDefaultLanguage = languageCode;
+        // Stop the loop when language matches
+        return false;
+      }
+
+      return true;
+    });
+  }
+
+  return currentDefaultLanguage;
+};
+
+export default createI18n({
+  locale: determineDefaultLanguage(),
+  legacy: false,
+  globalInjection: true, // Inject i18n properties and function to all components for options API
+  messages: translations
+});
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css';
+import i18n from './i18n';
 
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
@@ -10,5 +11,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(i18n);
 
 app.mount('#app');
